6b20f4243d73d96e544d85bc1abba611f43019c2
GeneralJenot <oszajcamichal@gmail.com>
2024-04-03 13:52:20 +0200

    Test i prowizoryczne wyświetlanie umiejętności

---
+++ components/Action_Description.gd
@@ -0,0 +1,6 @@
+extends RichTextLabel
+
+func _on_atak_button_down():
+	$Action_Description.text = str("fff")
+	$Action_Description.show()
+
---
+++ components/Stat_Buttons.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+
+func _on_atak_mouse_entered():
+	$Action_Description.text = str("fff")
+
+func _on_atak_mouse_exited():
+	$Action_Description.text = str(" ")
+
+func _on_atak_plus_mouse_entered():
+	$Action_Description.text = str("fff")
+
+func _on_atak_plus_mouse_exited():
+	$Action_Description.text = str(" ")
+
+func _on_obrona_mouse_entered():
+	$Action_Description.text = str("fff")
+
+func _on_obrona_mouse_exited():
+	$Action_Description.text = str(" ")
+
+func _on_obrona_plus_mouse_entered():
+	$Action_Description.text = str("fff")
+
+func _on_obrona_plus_mouse_exited():
+	$Action_Description.text = str(" ")
+
+func _on_special_mouse_entered():
+	$Action_Description.text = str("fff")
+
+func _on_special_mouse_exited():
+	$Action_Description.text = str(" ")
+
+func _on_special_2_mouse_entered():
+	$Action_Description.text = str("fff")
+
+func _on_special_2_mouse_exited():
+	$Action_Description.text = str(" ")
+
+func _on_special_3_mouse_entered():
+	$Action_Description.text = str("fff")
+
+func _on_special_3_mouse_exited():
+	$Action_Description.text = str(" ")
+
+func _on_special_4_mouse_entered():
+	$Action_Description.text = str("fff")
+
+func _on_special_4_mouse_exited():
+	$Action_Description.text = str(" ")
+
