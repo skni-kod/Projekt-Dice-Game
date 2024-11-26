77c4ac55f8380aaf393fad179a98186b689ff679
Sławek <slawekzdunek2.3.6@gmail.com>
2024-11-26 23:29:19 +0100

    Rerololling kostek

--- scripts/DiceManager.gd
+++ scripts/DiceManager.gd
@@ -1,19 +1,39 @@
 extends Node2D
+class_name DiceManager
 signal Potwierdzenie
 var dices = [];
+var rng = RandomNumberGenerator.new()
+var max_selected_dices = 3
+var current_max_selected_dices = 0
 
 func _ready() -> void:
 	for child in get_children():
 		dices.append(child as Dice)
-			
-
+	
+func SelectedDiceCount() -> int:
+	var c = 0
+	for dice in dices:
+		if dice.button_pressed:
+			c = c + 1
+	return c
+		
 func Roll() -> void:
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
+	for dice in dices:
+		dice.button_pressed = false
+			
 	for dice in dices:
 		dice.Randomize(rng)
+	
+	current_max_selected_dices = max_selected_dices
+		
 
-
+func Reroll() -> void:
+	current_max_selected_dices = current_max_selected_dices - SelectedDiceCount()
+	for dice in dices:
+		if dice.button_pressed:
+			dice.button_pressed = false
+			dice.Randomize(rng)
+			
 func Consume(faceTypes = []) -> bool:
 	var claimedDices = []
 	for i in range(dices.size()):
