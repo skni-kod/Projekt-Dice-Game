69e55fc3a617f4028456ad117319ea85ee70d792
Sławek <slawekzdunek2.3.6@gmail.com>
2024-12-11 22:36:47 +0100

    Poprawka Spawnera

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -6,7 +6,7 @@ var enemy: Enemy
 var spawner_licznik = 0
 
 func _ready():
-	spawner()
+	Spawn("goblin")
 	playerStats = get_node("/root/main_scene/Player") as Stats
 	diceManager = get_node("/root/main_scene/Dices") as DiceManager
 	
@@ -19,11 +19,10 @@ func EndEnemyTurn():
 
 func _on_enemy_die():
 	enemy.queue_free()
-	spawner()
+	Spawn("goblin")
 		
-##SPAWNER
-func spawner():
-	var enemy_prefab = load("res://prefabs/enemies/goblin.tscn")
+func Spawn(enemyName:String):
+	var enemy_prefab = load("res://prefabs/enemies/" + enemyName + ".tscn")
 	var enemy_instance = enemy_prefab.instantiate()
 	get_node("/root/main_scene").add_child(enemy_instance)
 	
