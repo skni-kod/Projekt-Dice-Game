2b6d3525ef40006d26be345140dc9675ab8d19d0
Sławek <slawekzdunek2.3.6@gmail.com>
2025-06-12 17:10:45 +0200

    Update Highscores.gd

--- scripts/Highscores.gd
+++ scripts/Highscores.gd
@@ -1,19 +1,16 @@
 extends Node
 
-# Called when the node enters the scene tree
 func _ready() -> void:
 	var http_request = HTTPRequest.new()
 	add_child(http_request)
 	http_request.connect("request_completed", Callable(self, "_on_request_completed"))
 
-	# Send a GET request
 	var url = "http://localhost:3001/highscores"
 	var error = http_request.request(url)
 	
 	if error != OK:
 		self.text = "Failed to send request. Error code: %d" % error
 
-# Signal handler for when the request is completed
 func _on_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
 	var response_text := body.get_string_from_utf8()
 
